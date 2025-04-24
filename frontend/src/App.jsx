104229fffa05bd15023a687e207b651e08169873
Yashyas <Ym130602@gmail.com>
2025-04-24 23:13:43 +0530

    Basic boilerplate of frontend complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,35 @@
-import { useState } from 'react'
-import { Routes,Route } from 'react-router-dom'
-
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import BookDetails from "./pages/BookDetails";
+import BookList from "./pages/BookList";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+import BookList from "./pages/BookList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   
-      <Routes>
-        <Route path="/" element = {<HomePage/>}/>
-      </Routes>
-  
-
-  )
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/books/:id" element={<BookDetails />} />
+      <Route path="/books" element={<BookList />} />
+      <Route
+        path="/profile"
+        element={
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
